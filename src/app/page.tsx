51771b7441fbc92388098bad91c92582c9f5387e
최유리 <choeyuli@gimgyeoglogsMBP.lan>
2023-12-17 18:10:27 +0900

    ver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>홈페이지다!!</h1>
+      <h1>홈페이지다!! 버전2</h1>
       <Counter />
       <Image
         src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
